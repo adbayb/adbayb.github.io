b94a74d1c8e95bfba528a9e4d100c9d9e1c49cab
Ayoub Adib <adbayb@gmail.com>
2023-07-31 01:10:42 +0200

    refactor: fix polymorphism issues and create helper

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -1,53 +1,26 @@
-import {
-	type ComponentProps,
-	type JSX,
-	type ValidComponent,
-	splitProps,
-} from "solid-js";
+import { type JSX, splitProps } from "solid-js";
 import { Dynamic } from "solid-js/web";
+import { createPolymorphicComponent } from "../createPolymorphicComponent";
 import { type Styles, styleKeys, styles } from "./styles.css";
 
 export interface ViewProps extends Styles {
 	children: JSX.Element;
 }
 
-export const View: PolymorphicComponent<ViewProps> = ({
-	as = "div",
-	children,
-	...restProps
-}) => {
-	const [styleProps, otherProps] = splitProps(restProps, styleKeys);
+const DEFAULT_AS = "div";
 
-	return (
-		<Dynamic
-			{...otherProps}
-			component={as}
-			class={styles(styleProps)}
-		>
-			{children}
-		</Dynamic>
-	);
-};
+export const View = createPolymorphicComponent<ViewProps, typeof DEFAULT_AS>(
+	({ as = DEFAULT_AS, children, ...restProps }) => {
+		const [styleProps, otherProps] = splitProps(restProps, styleKeys);
 
-type PropsOf<As extends ValidComponent> = ComponentProps<As>;
-
-/**
- * Utility type to create a polymorphic component
- */
-type PolymorphicComponent<OwnProps extends object> = <
-	As extends ValidComponent = keyof JSX.IntrinsicElements,
->(
-	props: PolymorphicProps<As, OwnProps>
-) => JSX.Element;
-
-/**
- * Utility type to create polymorphic props by:
- * - Inheriting props from the own provided consumer-side (eg. ViewOwnProps)
- * - Inferring props from the defined `as` element
- */
-type PolymorphicProps<
-	As extends ValidComponent,
-	OwnProps extends object,
-> = OwnProps & {
-	as: As;
-} & Omit<PropsOf<As>, keyof OwnProps | "as">;
+		return (
+			<Dynamic
+				{...otherProps}
+				component={as}
+				class={styles(styleProps)}
+			>
+				{children}
+			</Dynamic>
+		);
+	}
+);
---
+++ src/components/createPolymorphicComponent.ts
@@ -0,0 +1,33 @@
+import type { Component, ComponentProps, JSX } from "solid-js";
+
+type AnyElement = keyof JSX.IntrinsicElements | Component<any>;
+
+type PropsOf<As extends AnyElement> = ComponentProps<As>;
+
+/**
+ * Helper to create a polymorphic component
+ */
+export const createPolymorphicComponent = <
+	OwnProps extends object,
+	DefaultAs extends AllowedAs,
+	AllowedAs extends AnyElement = AnyElement,
+>(
+	Component: (props: PolymorphicProps<OwnProps, DefaultAs>) => JSX.Element
+) => {
+	return Component as {
+		<As extends AllowedAs>(props: PolymorphicProps<OwnProps, As>): JSX.Element;
+		(props: Omit<PolymorphicProps<OwnProps, DefaultAs>, "as">): JSX.Element;
+	};
+};
+
+/**
+ * Utility type to create polymorphic props by:
+ * - Inheriting props from the own provided consumer-side (eg. ViewOwnProps)
+ * - Inferring props from the defined `as` element
+ */
+type PolymorphicProps<
+	OwnProps extends object,
+	As extends AnyElement,
+> = OwnProps & {
+	as: As;
+} & Omit<PropsOf<As>, keyof OwnProps | "as">;
